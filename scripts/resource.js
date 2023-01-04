f745debfa0d37644551cb784208f8b52e59ee7d6
Aahan341 <aaahan006@gmail.com>
2023-01-04 11:42:50 +0530

    day3

--- scripts/resource.js
+++ scripts/resource.js
@@ -1,8 +1,8 @@
-import navbar from "./component/navbar.js"
+import {navbar} from "./Component/resourcesnavbar.js"
 
 let header=document.getElementById("navbar_section")
 header.innerHTML = navbar()
-console.log(navbar())
+
 // console.log(navbar());
 // document.getElementById("logo").onclick=()=>{
 //     window.location.href="resources.html" 
